cc9c650e4c0908a87da60636519cc103c11240b5
Ryan Todd <rtoddinquiries@gmail.com>
2025-02-10 21:08:36 -1000

    14 minute WOD
    Took me two attempts

---
+++ ziplist.ts
@@ -0,0 +1,24 @@
+function zipList(list1: unknown[], list2: unknown[]): unknown[]{
+    const newList = [];
+    if(!(list1.length === list2.length)){
+        console.log("Both lists need to be the same length")
+        return [];
+    }
+    for(let i = 0; i < list1.length; i++){
+        newList.push(list1[i]);
+        newList.push(list2[i]);
+    }
+    return newList;
+}
+
+function zipListTheFunctionalWay(list1: unknown[], list2: unknown[]): unknown[] {
+    const result: unknown[] = [];
+    list1.forEach((element, index) => {
+        result.push(element, list2[index]);
+    });
+    return result;
+}
+
+
+console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
+console.log(zipListTheFunctionalWay(['a', 'b', 'c'], [1, 2, 3]));
